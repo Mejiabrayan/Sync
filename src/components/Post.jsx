090fd7b7dd2c15d5e6c42fe6f44feb67e7a81dd0
mejiabrayan <learnwithbrayan@gmail.com>
2023-02-22 13:27:12 -0800

    Update Post component to fetch author from API and add Like button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import {BsGrid3X3Gap} from 'react-icons/bs';
+import React, { useState, useEffect } from 'react';
+import { BsGrid3X3Gap } from 'react-icons/bs';
+import axios from 'axios';
 
 function Post({ post, onLike }) {
+  const [author, setAuthor] = useState('Unknown');
+
+  useEffect(() => {
+    async function fetchAuthor() {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/users/${post.userId}`
+      );
+      setAuthor(response.data.name);
+    }
+    fetchAuthor();
+  }, [post.userId]);
+
   return (
     <div className="p-4 mb-4 border rounded-md">
-      <div className="flex items-center justify-between mb-2">
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="text-lg font-medium">{post.author}</h2>
-          <img src='https://via.placeholder.com/50' alt='Profile' className='w-8 h-8 rounded-full' />
+      <div className="flex flex-col sm:flex-row items-center justify-between mb-2">
+        <div className="flex flex-col sm:flex-row items-center justify-center mb-2 sm:mb-0">
+          <img src='https://via.placeholder.com/50' alt='Profile' className='w-8 h-8 rounded-full mr-2' />
+          <h2 className="text-lg font-medium">{author}</h2>
         </div>
         <button className="bg-blue-500 text-white px-3 py-1 rounded-md" onClick={() => onLike(post.id)}>
           Like ({post.likes})
         </button>
       </div>
-      <p>{post.content}</p>
+      <p>{post.body}</p>
     </div>
   );
 }
